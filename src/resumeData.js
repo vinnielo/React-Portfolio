59ad7456ecf256323edbfea4e584f65826d41cd6
jose Lopez <vinnielo01@gmail.com>
2020-06-24 12:54:14 -0700

    added react app files

---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "logo": "../../images/Vinnie-Lopez-white-high-res.png",
+    "name": "Vinnie Lopez",
+    "role": "FullStack Web Developer",
+    "linkedinId":"https://www.linkedin.com/in/jose-lopez-8397ba92/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jose-lopez-8397ba92/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/vinnielo",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"",
+    "address":"Los Angeles, CA",
+    "website":"https://vinnielo.github.io",
+    "education":[
+      {
+        "UniversityName":"UCLA Extension",
+        "specialization":"Full Stack Web Developing ",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2020",
+      
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Westlake Financial Services",
+        "specialization":"Business Development Lead",
+        "MonthOfLeaving":"open",
+        "YearOfLeaving":"",
+        "Achievements":"Employee of the Year 2007"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"MongoJS/Mongoose"
+      }
+
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
