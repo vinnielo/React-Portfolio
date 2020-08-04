7b38e82c7f57c1fc28bdd9f6c2cffbfc7350630a
Vinnie Lopez <vinnielo01@gmail.com>
2020-08-04 13:30:04 -0700

    Updates

---
+++ precache-manifest.05dfc6d2aec61f32a8f52e35ad32111c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dcb758a24c7cd81b1d2",
+    "url": "/static/css/main.e2a5f06e.chunk.css"
+  },
+  {
+    "revision": "5dcb758a24c7cd81b1d2",
+    "url": "/static/js/main.5dcb758a.chunk.js"
+  },
+  {
+    "revision": "732ca97d9dc577b5237d",
+    "url": "/static/js/1.732ca97d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f7b9ffc74da0368220d90a89ac8e3f25",
+    "url": "/index.html"
+  }
+];
